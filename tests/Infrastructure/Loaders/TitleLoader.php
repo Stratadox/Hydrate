e494f1abb389222fbf9c75e2517344026a1ea495
Stratadox <jesse@stratadox.com>
2017-11-26 17:39:04 +0100

    Move title loading query to proper method

--- tests/Infrastructure/Loaders/TitleLoader.php
+++ tests/Infrastructure/Loaders/TitleLoader.php
@@ -5,8 +5,8 @@ declare(strict_types=1);
 namespace Stratadox\Hydrate\Test\Infrastructure\Loaders;
 
 use SQLite3;
+use SQLite3Result;
 use Stratadox\Hydrate\Test\Model\Book;
-use Stratadox\Hydrate\Test\Model\Title;
 use Stratadox\Hydration\Hydrates;
 use Stratadox\Hydration\Proxying\Loader;
 
@@ -28,10 +28,14 @@ class TitleLoader extends Loader
 
     protected function doLoad($book, string $property, $chapter = null)
     {
-        return $this->loadTheTitleOfTheChapterInThe($book, $chapter);
+        $result = $this->fetchTheTitleOfTheChapterInThe($book, $chapter);
+        $title = $this->hydrateTheTitle->fromArray(
+            $result->fetchArray(SQLITE3_ASSOC)
+        );
+        return $title;
     }
 
-    private function loadTheTitleOfTheChapterInThe(Book $book, int $chapter = null) : Title
+    private function fetchTheTitleOfTheChapterInThe(Book $toRead, int $chapter = null) : SQLite3Result
     {
         $query = $this->database->prepare(
             'SELECT `chapter`.`title` as `title`
@@ -41,12 +45,8 @@ class TitleLoader extends Loader
                     `content`.`chapter_number` = :chapter
                 );'
         );
-        $query->bindValue('book', $book->isbn());
+        $query->bindValue('book', $toRead->isbn());
         $query->bindValue('chapter', $chapter);
-
-        $title = $this->hydrateTheTitle->fromArray(
-            $query->execute()->fetchArray(SQLITE3_ASSOC)
-        );
-        return $title;
+        return $query->execute();
     }
 }
